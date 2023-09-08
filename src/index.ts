d9c0eb073177df86e5a659d72aba5ba391bc9c18
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2023-09-07 22:33:08 -0400

    udate script

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,10 @@ const REVIEW =
   "https://app.schoology.com/course/6268682396/materials?f=640388156";
 const POST_AP =
   "https://app.schoology.com/course/6268682396/materials?f=640388190";
+const ALBERT =
+  "https://app.schoology.com/course/6268682396/materials?f=640387911";
+const TESTS =
+  "https://app.schoology.com/course/6268682396/materials?f=640387920";
 
 const units = [
   { name: "introduction", link: INTRODUCTION },
@@ -34,16 +38,18 @@ const units = [
   { name: "vector_polar", link: VECTOR_POLAR },
   { name: "review", link: REVIEW },
   { name: "postAP", link: POST_AP },
+  { name: "albert", link: ALBERT },
+  { name: "tests", link: TESTS },
 ];
 
 const browser = await puppeteer.launch({ headless: false });
 const page = await browser.newPage();
 
+// Login
 await page.goto("https://app.schoology.com/login");
 await page.setViewport({ width: 1080, height: 1024 });
 await page.type("#edit-mail", process.env.SCHOOLOGY_EMAIL!);
 await page.type("#edit-pass", process.env.SCHOOLOGY_PASSWORD!);
-
 const loginButton = "#edit-submit";
 await page.waitForSelector(loginButton);
 await page.click(loginButton);
@@ -55,8 +61,7 @@ for (const unit of units) {
     ".item-title > a",
     (elements: HTMLAnchorElement[]) => {
       return elements.reduce<string[]>(
-        (prev, curr) =>
-          curr.innerText.substring(0, 2) == "HW" ? [curr.href, ...prev] : prev,
+        (prev, curr) => [curr.href, ...prev],
         []
       );
     }
@@ -90,6 +95,7 @@ for (const unit of units) {
       if (!fs.existsSync(attachmentsFolder)) {
         fs.mkdirSync(attachmentsFolder, { recursive: true });
       }
+      // download attachments
       for (const url of urls) {
         try {
           await page.goto(url[1]);
@@ -99,10 +105,36 @@ for (const unit of units) {
           // this errors a bunch but it still works
         }
       }
-      await page.goBack();
     } catch (err) {
       console.error(err);
     }
+  }
+}
+
+//Quote of the day
+const QUOTE_OF_THE_DAY =
+  "https://app.schoology.com/course/6268682396/materials?f=640386557";
+await page.goto(QUOTE_OF_THE_DAY);
+const quoteLinks = await page.$$eval(".item-title > a", (elements) =>
+  elements.map((element) => element.href)
+);
+for (let i = 0; i < quoteLinks.length; i++) {
+  try {
+    await page.goto(quoteLinks[i]);
+    const [pageTitle, html] = await Promise.all([
+      page.$eval(".page-title", (element: any) => element.innerText),
+      page.$eval(".s-page-content-full", (element) => element.innerHTML),
+    ]);
+    const folder = path.join(
+      __dirname,
+      "../content",
+      `quotes_of_the_day/${i}_${pageTitle.replaceAll(" ", "_")}`
+    );
+    if (fs.existsSync(folder)) continue;
+    fs.mkdirSync(folder, { recursive: true });
+    fs.writeFileSync(path.join(folder, "quote.html"), html);
+  } catch (err) {
+    console.error(err);
   }
 }
 
